23bde24322e6c4544f71cd8909f283ea0bc97409
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-20 06:55:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <studio.h>
 /**
- * My first C script
+ * Main - prints ("with proper grammar, but the outcome is a piece of art, followed by a newline)
+ * Return: Always 0 (Success)
  *
+ * printf command
  */
-
-int main(void)
+int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
