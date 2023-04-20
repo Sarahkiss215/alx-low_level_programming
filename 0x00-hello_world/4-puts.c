1dc0a6b34a9fe308037d5a7196a46f14d3600a75
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-20 07:18:26 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - puts(""Programming is like building a multilingual puzzle \n")
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
