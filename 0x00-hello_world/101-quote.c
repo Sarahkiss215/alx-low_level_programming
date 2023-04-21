d7e7101d108599586a122fbc023b85b2e4b35318
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-21 03:15:17 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+ * no printf and puts commands
+ * Return (1)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
