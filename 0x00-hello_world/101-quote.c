af2a8d03acda3e731c024134adf61c4bc26cfae2
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-21 03:34:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
  * no printf and puts commands
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
