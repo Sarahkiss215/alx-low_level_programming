228a283ce1c3b821c86e49c94c9db1e17060b2c7
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-25 16:23:52 -0700

    Alphabetin lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower and upper cases
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+	char ALPHABET;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar (alphabet);
+	for (ALPHABET = 'A'; ALPHABET <= 'Z';ALPHABET++)
+		putchar (ALPHABET);
+	putchar('\n');
+
+	return (0);
+}
