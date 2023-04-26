bd6b2b92e8d87f1b5846dbae4d2d035e9de1ef10
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 06:17:24 -0700

    Added 1 inside for statement

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			for (c = 0; c <= 9; c++)
 			{
-				for (d = 1; d <= 9; d++)
+				for (d = 0; d <= 9; d++)
 				{
 					putchar ((a % 10) + '0');
 					putchar ((b % 10) + '0');
@@ -26,7 +26,7 @@ int main(void)
 					putchar ((c % 10) + '0');
 					putchar ((d % 10) + '0');
 
-					if (!(a == 9 && b == 8))
+					if (a == 0 && b == 0 && c == 0 && d != 0)
 					{
 					putchar (',');
 					putchar (' ');
