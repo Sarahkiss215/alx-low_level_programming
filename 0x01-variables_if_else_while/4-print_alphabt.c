48a6472dc9f9da58904111150b7e8c04bd4c6e8c
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 00:16:55 -0700

    Alphabet letters with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabet letters except q && e
+ * followed by new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar (alphabet);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
+
