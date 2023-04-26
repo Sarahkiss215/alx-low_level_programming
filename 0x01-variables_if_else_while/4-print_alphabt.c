843d07bd96d734862aec6e17cf1c82b05afae5cb
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 00:21:32 -0700

    Alphabet letters with exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,8 @@
 int main(void)
 {
 	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet ='a'; alphabet <='z'; alphabet++)
 	{
 		if (alphabet != 'e' && alphabet != 'q')
 			putchar (alphabet);
