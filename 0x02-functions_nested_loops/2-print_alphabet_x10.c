07b6bc9f3a5119e27f3e16bdb3f53b7f68e066d0
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 14:58:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,11 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alphabet;
 
-	while (count++ < 10)
+	while (count <= 9)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 		_putchar('\n');
+		count++;
 	}
 }
