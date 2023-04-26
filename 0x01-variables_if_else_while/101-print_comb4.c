868c83258712ba8938c8c40478ecdbb0391d3f60
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 05:09:49 -0700

    combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of three digits
+ * digits separated by comma then space
+ * followe by newline
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <=8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar ((a % 10) + '0');
+				putchar ((b % 10) + '0');
+				putchar ((c % 10) + '0');
+
+				if (a == 7 && b == 8 && c == 9)
+					continue;
+
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
