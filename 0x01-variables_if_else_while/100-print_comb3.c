26218493e81a3b78fec62ef52bfafdffb64f033b
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-26 04:11:04 -0700

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,19 @@ int main(void)
 	int b;
 
 	for (a = 0; a <= 8; a++)
-	for (b = 1; b <= 9; b++)
-		putchar (a);
-		putchar (b);
-		if (a != 1 && b != 0)
+	{
+		for (b = a + 1; b <= 9; b++)
 		{
+			putchar ((a % 10) + '0');
+			putchar ((b % 10) + '0');
+		
+			if (a != 1 && b != 0)
+				continue;
+		
 			putchar (',');
 			putchar (' ');
 		}
+	}
 	putchar ('\n');
 
 	return (0);
