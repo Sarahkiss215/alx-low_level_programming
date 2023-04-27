ac0bc0fa10a276606af165e461cb57d0163059bb
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-27 15:44:41 -0700

    Replaced for loop with while loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,8 +9,11 @@ void print_numbers(void)
 {
 	int num;
 	
-	for (num = 0; num <= 9; num++)
-		_putchar(num + '0');
-	
+	num = 0;
+	while (num <= 9)
+	{
+		_putchar(num + '0')
+		num++;
+	}
 	_putchar('\n');
 }
