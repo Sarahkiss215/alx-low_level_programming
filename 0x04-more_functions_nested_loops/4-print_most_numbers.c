9797734527e5bda26b5cc96a9905ad4ab3c352e2
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-27 16:24:02 -0700

    rearranged my code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,8 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar((num % 10) + '0');
 		if (num != 2 && num != 4)
-			continue;
+		_putchar((num % 10) + '0');
 	}
 	_putchar('\n');
 }
