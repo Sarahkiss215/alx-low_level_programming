0713ef3dd93045db734529feb0ca423badfd84ea
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-27 05:25:52 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,19 +5,19 @@
  * Return: 0
  */
 
+
 int main(void)
+
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	while (i < 10)
-	
-	{
-		putchar (i);    /*The int i never never increments so will always print 0 and less than 10*/
-	}
-	
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar (i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
