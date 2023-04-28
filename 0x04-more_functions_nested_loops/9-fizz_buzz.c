b1b0881c114027b71f0fda2cf5e17d5b55f064c0
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-28 02:50:01 -0700

    replace the initia value of i to 0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i == 100)
+			continue;
+	printf(" ");
 	}
 	printf("\n");
 	return (0);
