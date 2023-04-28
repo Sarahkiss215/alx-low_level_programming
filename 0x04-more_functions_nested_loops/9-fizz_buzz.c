d8b1651b628253852825d98d9e45b7c6e8385f0e
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-28 02:41:49 -0700

    included the standard I/O header

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,13 @@ int main(void)
 	for (i = 0; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else
-			printf("%d", i);
-		if (i == 100)
-			continue;
-		printf(" ");
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
