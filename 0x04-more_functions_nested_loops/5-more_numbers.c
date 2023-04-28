d205ecf42375b20c3ebb73924eaa62e66ed33bc8
Sarahkiss215 <skisiangani5@gmail.com>
2023-04-27 17:06:29 -0700

    print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		j = 0;
 		while (j <= 14)
@@ -19,5 +19,6 @@ void more_numbers(void)
 			j++;
 		}
 		_putchar('\n');
+		i++
 	}
 }
