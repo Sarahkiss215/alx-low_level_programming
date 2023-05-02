7642b63a367a8513366e29845f1e7d2962f7e08d
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-02 05:39:35 -0700

    replaced a minus with a plus

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	if ((length % 2) == 0)
 		n = length / 2;
 	else
-		n = (length - 1) / 2;
+		n = (length + 1) / 2;
 	for (digit = n; digit < length; digit++)
 		_putchar(str[digit]);
 	_putchar('\n');
