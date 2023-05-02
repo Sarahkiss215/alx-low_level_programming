712686f2a4bb1bf03f8ca527a0aeb4f9cfacbb8e
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-02 04:14:37 -0700

    removes whitespaces

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,19 @@
  */
 
 void rev_string(char *s)
-{       
-        int letters, length;
-        char temp;
-        
-        letters = 0;
-        length = 0;
-        while (s[letters++])
-                length++;
-        
-        for (letters = length -1; letters >= length / 2; letters--)
-        {       
-                temp = s[letters];
-                s[letters] = s[length - letters - 1];
-		s[length -letters - 1] = temp;
+{
+	int letters, length;
+	char temp;
+
+	letters = 0;
+	length = 0;
+	while (s[letters++])
+		length++;
+
+	for (letters = length - 1; letters >= length / 2; letters--)
+	{
+		temp = s[letters];
+		s[letters] = s[length - letters - 1];
+		s[length - letters - 1] = temp;
 	}
 }
