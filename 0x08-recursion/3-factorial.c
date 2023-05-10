dfa52fd63a2fd9e945a667c01a51ec0851325751
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-10 02:35:23 -0700

    factorization

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number
+ * @n: number to be used
+ *
+ * Return: -1 if n<0, factorial otherwise
+ */
+
+int factorial(int n)
+{
+	int i = 1;
+	int fact = n;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return(1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	fact = fact * factorial(n - i);
+	return (fact);
+}
