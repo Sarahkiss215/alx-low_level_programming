01004b4862b89e994a759a629fa773872212c724
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-15 04:42:08 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointed to newly allocated space
+ * @str: string to be used
+ *
+ * Return: NULL if str is NULL,NULL if insufficient memory
+ * and a pointer to the duplicate str
+ */
+char *_strdup(char *str)
+{
+	int n = 0;
+	int size = 0;
+	char *newstr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; str[n]; n++)
+	{
+		size++;
+	}
+	newstr = (char *)malloc(sizeof(char) * (size + 1));
+
+	for (; str[n]; n++)
+	{
+		newstr[n] = str[n];
+	}
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	
+	return (newstr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,3 @@
 void simple_print_buffer(char *buffer, unsigned int size);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
