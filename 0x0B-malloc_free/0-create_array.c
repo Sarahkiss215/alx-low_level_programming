c338502f6c0c033fe201af9c968f036cec8cc592
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-15 04:57:29 -0700

    malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
 	for (n = 0; n < size; n++)
 	{
