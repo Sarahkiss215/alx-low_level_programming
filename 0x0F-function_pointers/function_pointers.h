b53262c5b3719980518b5a68c779332fff2c75f8
Sarahkiss215 <skisiangani5@gmail.com>
2023-05-24 03:58:01 -0700

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,10 +5,7 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
-void print_elem(int elem);
-void print_elem_hex(int elem);
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
