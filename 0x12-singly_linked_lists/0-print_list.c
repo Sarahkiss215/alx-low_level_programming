8a26071e4efd2d2adf730bc7673fa08d2ac0b204
Sarahkiss215 <skisiangani5@gmail.com>
2023-06-02 01:46:26 -0700

    printing members of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: members of list_t
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
