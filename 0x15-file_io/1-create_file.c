b8ec82c0887d62eb77354196f1195cb052f18d9b
Sarahkiss215 <skisiangani5@gmail.com>
2023-08-09 00:29:27 -0700

    Add read and write permissions to user

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	/* S-IR read permission for owner, S_IWwrite permission for owner*/
-	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC,  S_IRUSR | S_IWUSR);
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC,  0600);
 	wr = write(op, text_content, length);
 	if (op == -1)
 	{
