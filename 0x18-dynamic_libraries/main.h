87cc3bea8112010f3abf8655b1b851b29956d174
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-18 08:44:34 -0700

    Remove one _putchar prototype

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -25,6 +25,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _putchar(char c);
+
 
 #endif/* MAIN_H */
