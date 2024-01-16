a8456c979624b995bc55c3bbc4c15910a07f25c8
Sarahkiss215 <skisiangani5@gmail.com>
2024-01-16 01:44:32 -0800

    Linear search in a skip list

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: pointer to the head of the skip list to search in
+ * @value: value to be searched for
+
+ * Return: pointer to first node containing value or NULL if list is empty
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node = list;
+
+	if (list == NULL)
+		return (NULL);
+
+	while (node->next != NULL)
+	{
+		list = node;
+
+		if (node->express)
+		{
+			node = node->express;
+		}
+		else
+		{
+			while (node->next)
+				node = node->next;
+			break;
+		}
+
+		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+
+		if (node->n >= value)
+			break;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+	       node->index);
+
+	while (list != NULL && list != node->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,7 +21,25 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
+skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 void int_array(int *array, int size);
